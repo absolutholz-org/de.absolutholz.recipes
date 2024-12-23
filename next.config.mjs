b5380c8a12ccd0c21d8d0b55f9cbd4a3466ca2ea
absolutholz <absolutholz@gmail.com>
2024-12-23 12:31:21 +0100

    deploy fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // Optional: Add trailing slash to all generated URLs
+  trailingSlash: true,
+  // Optional: Change the output directory (default is 'out')
+  distDir: "dist", // Example: Change output to 'dist'
+  // ... other configurations
+};
 
 export default nextConfig;
